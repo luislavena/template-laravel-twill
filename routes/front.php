c7568d11f73dadb97dde89717c343e7745b494f4
Antonio Ribeiro <antonio@area17.com>
2022-07-18 12:09:13 +0200

    Properly configure middleware for front

--- routes/front.php
+++ routes/front.php
@@ -6,16 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [Homepage::class, 'redirectToLocalizedHomepage'])->name('homepage.redirect');
 
-Route::group(
-    [
-        'prefix' => '{locale}',
-        'name' => 'front',
-        'middleware' => ['auth.very_basic', 'url.slash'],
-        'where' => ['locale' => locales()->join('|')],
-    ],
-    function () {
+Route::name('front.')
+    ->prefix('{locale}')
+    ->middleware(['http-basic-auth', 'url.slash'])
+    ->where(['locale' => locales()->join('|')])
+    ->group(function () {
         Route::get('/', [Homepage::class, 'index'])->name('homepage');
 
         Route::get('/pages/{slug}', [Pages::class, 'show'])->name('pages.show');
-    },
-);
+    });
