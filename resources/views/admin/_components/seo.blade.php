0c2e5459df438e83e82000125c1107f0eb434b47
Antonio Ribeiro <antonio@area17.com>
2022-07-18 15:34:18 +0200

    Simplify SEO

--- resources/views/admin/_components/seo.blade.php
+++ resources/views/admin/_components/seo.blade.php
@@ -1,28 +1,4 @@
 <a17-fieldset title="{{__('SEO')}}" id="seo" :open="false">
-    @if ($withEditorial ?? false)
-        @formField('input', [
-            'label' => __('Editorial title'),
-            'name' => 'seo_editorial_title',
-            'type' => 'text',
-            'translated' => true
-        ])
-
-        @formField('input', [
-            'label' => __('Editorial text'),
-            'name' => 'seo_editorial_text',
-            'rows' => 4,
-            'type' => 'textarea',
-            'translated' => true
-        ])
-    @endif
-
-    @if ($withNoIndex ?? false)
-        @formField('checkbox', [
-            'name' => 'seo_noindex',
-            'label' => 'Do not index that page'
-        ])
-    @endif
-
     @formField('input', [
         'label' => __('Meta title'),
         'name' => 'seo_title',
